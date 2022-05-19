ca9bf95a3bcb498641428557fa37e5ac8c64bcfa
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-19 19:18:49 +0700

    add messages from UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
 
   def new
   end
 
   def create
-    if user = user.authenticate(params[:session][:password])
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
@@ -17,8 +19,17 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     @_current_user = nil
-    # flash[:success] = "You have successfully logged out"
-    redirect_to login_path, success: "You have successfully logged out"
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
+  private
+  
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
