c593f6063b66075ee5890cca9bce6fd98cb907c3
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-05-19 18:18:07 +0700

    add authentication system; enable flash messages display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
-    
   end
+
+  def create
+    if user = user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @_current_user = nil
+    # flash[:success] = "You have successfully logged out"
+    redirect_to login_path, success: "You have successfully logged out"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :messages
 end
